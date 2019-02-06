58d8a0e098dd17d85757cac8ca6f83d5e0c4cffa
Brian Balderston <brian.j.balderston.civ@mail.mil>
2019-02-06 13:03:45 -0500

    Add claims

--- AuthorizationLab/Controllers/HomeController.cs
+++ AuthorizationLab/Controllers/HomeController.cs
@@ -7,7 +7,8 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace AuthorizationLab.Controllers
 {
-    [Authorize(Roles = "Administrator")]
+    [Authorize(Policy = "AdministratorOnly")]
+    [Authorize(Policy = "EmployeeId")]
     public class HomeController : Controller
     {
         public IActionResult Index()
